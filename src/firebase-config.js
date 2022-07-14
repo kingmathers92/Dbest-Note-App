b01eeac7191afb110626339512d5a3fc26c21cba
khaled <khaledb.yahya@gmail.com>
2022-07-14 21:10:02 +0100

    connecting to firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+
+  projectId: process.env.REACT_APP_PROJECTID,
+
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+
+  messagingSenderId: process.env.REACT_APP_MSGID,
+
+  appId: process.env.REACT_APP_APPID,
+
+  measurementId: process.env.REACT_APP_MEASID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
